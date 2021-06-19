db4c087fe254977d5964f7a8508e9f8912dc209b
Evangelista Grace <42605267+evangelistagrace@users.noreply.github.com>
2021-06-19 21:43:17 +0800

    Update react.js

--- react.js
+++ react.js
@@ -73,3 +73,17 @@ function App() {
 
 
 export default App;
+
+
+// index.js //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
